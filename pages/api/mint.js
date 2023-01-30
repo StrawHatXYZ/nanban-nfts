0a7a3de8a32b3bc647ca114399f6eb5a0f89f471
saikishore222 <saikishore.chsk@gmail.com>
2023-01-30 11:00:10 +0530

    updated Nft Editon

--- pages/api/mint.js
+++ pages/api/mint.js
@@ -2,14 +2,36 @@ const path = require("path");
 import { exec } from "child_process";
 
 export default function handler(req, res) {
-  const { signer } = req.query;
-
+  console.log(req.query);
+  console.log(req.query.edition);
+  console.log(req.query.signer);
+  const  {signer}  = req.query;
+  const edvalue=req.query.edition;
+  let Edition;
+  if(edvalue=='Silver')
+  {
+    Edition=1;
+  }
+  else if(edvalue=='Gold')
+  {
+    Edition=2;
+  }
+  else if(edvalue=='Diamond')
+  {
+    Edition=3;
+  }
+  else
+  {
+    console.log(edvalue);
+  }
   // pick 1 from n total
-  const randomEdition = 2;
+  let sc=0;
+  let gc=0;
+  let dc=0;
 
   const nftDataFile = path.join(
     process.env.NFT_COLLECTION_PATH,
-    `${randomEdition}-nft.json`
+    `${Edition}-nft.json`
   );
 
   const commandstr = `metaboss mint one --keypair ${process.env.MERCHANT_KEYPAIR_PATH} --nft-data-file ${nftDataFile} --receiver ${signer} --rpc ${process.env.NEXT_PUBLIC_RPC_URL}`;
@@ -28,11 +50,25 @@ export default function handler(req, res) {
     }
 
     if (stdout) {
-      console.log("success", stdout);
+      let data=stdout.split(" ");
+      let mintAccount=data[4].split("\n");
+      console.log(mintAccount[0]);
+      if(Edition==1)
+      {
+        sc=sc+1;
+      }
+      if(Edition==2)
+      {
+        gc=gc+1;
+      }
+      if(Edition==3)
+      {
+        dc=dc+1;
+      }
     }
 
     if (success) {
-      res.status(200).json({ signer: signer, success: true });
+      res.status(200).json({ signer: req.query.signer, success: true });
     } else {
       res.status(500).json({ success: false, stderr: stderr });
     }
